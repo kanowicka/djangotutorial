58e5904e80b08ab0d8897ba37eb27bb314332a45
knowicka <nowicka.karolina@icloud.com>
2022-07-16 16:17:49 +0200

    migrations change

--- polls/models.py
+++ polls/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
+    question_text = models.CharField(max_length=150)
     pub_date = models.DateTimeField('date published')
 
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
+    choice_text = models.CharField(max_length=150)
     votes = models.IntegerField(default=0)
 
