d51f94a1ef9358d4d6200f4c4ce5b4d89ba7c28e
knowicka <nowicka.karolina@icloud.com>
2022-07-19 20:50:34 +0200

    poprawki

--- polls/views.py
+++ polls/views.py
@@ -5,22 +5,20 @@ from django.shortcuts import get_object_or_404, render
 
 from .models import Question
 
+
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, 'polls/detail.html', {'question': question})
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
+
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
 
+
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -28,5 +26,5 @@ def index(request):
     context = {
         'latest_question_list': latest_question_list,
     }
+
     return HttpResponse(template.render(context, request))
-# Create your views here.
